9870c709b13c0c50c5191bfd06d830acc88f1a55
chao <chao@mgchao.top>
2025-06-30 19:35:54 +0800

    fix: 移除无效代码

--- src/app/fixedContent/components/fixedContentCore/index.tsx
+++ src/app/fixedContent/components/fixedContentCore/index.tsx
@@ -939,15 +939,6 @@ export const FixedContentCore: React.FC<{
                     cursor: grabbing;
                 }
 
-                .fixed-image-container-thumbnail-drag-region {
-                    position: absolute;
-                    top: 0;
-                    left: 0;
-                    width: 100%;
-                    height: 100%;
-                    z-index: 99999999;
-                }
-
                 .fixed-html-content,
                 .fixed-text-content {
                     transform-origin: top left;
